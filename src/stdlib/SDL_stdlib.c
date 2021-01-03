36696b5eb5359ddad72d4c57daf1cadd80dd17ff
Kiharu Shishikura <mutobow0414@yahoo.co.jp>
2021-01-03 14:15:34 +0700

    Update SDL_stdlib.c

--- src/stdlib/SDL_stdlib.c
+++ src/stdlib/SDL_stdlib.c
@@ -549,11 +549,11 @@ localexit:
     /* *INDENT-ON* */
 }
 
-void
-_ftol2_sse()
-{
-    _ftol();
-}
+//void
+//_ftol2_sse()
+//{
+//    _ftol();
+//}
 
 /* 64-bit math operators for 32-bit systems */
 void
